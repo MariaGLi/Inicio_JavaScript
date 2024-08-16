bd911cd72cc464a5514482d6de1172fdf34bfa44
MariaGLi <lizarazoleal27@gmail.com>
2024-08-16 11:48:10 -0500

    :sparkles: Aprendiendo JS.

---
+++ Script.js
@@ -0,0 +1,30 @@
+//Impresión en consola
+console.log("Hola tibuyanos.")
+
+//Tipo de dato - Number
+numero1=1;
+numero2=3.14;
+
+//Verificación de tipo de dato.
+console.log(typeof numero1);
+console.log(typeof numero2);
+
+//Operaciones
+operacion=((((5**6)+256)/5)*2)
+console.log(operacion);
+
+//String
+texto1="Campus";
+texto2="Lands";
+
+//Concatenación
+console.log(texto1+texto2)
+
+//Booleanos
+booleano1= true;
+booleano2= false;
+booleano3= false;
+booleano4= true;
+operadoresBandera=(booleano1||booleano2)&(booleano3||booleano4)
+
+console.log(operadoresBandera);
